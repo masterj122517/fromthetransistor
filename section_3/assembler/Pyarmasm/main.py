d7eab632a170566af6e036b14cc5c0d1ce8b797c
masterj <masterj122517@163.com>
2025-10-29 20:57:59 +0800

    SECTION_3 TO BE CONTINUE

--- section_3/assembler/Pyarmasm/main.py
+++
@@ -1,8 +0,0 @@
-from lexer import Lexer
-
-lexer = Lexer()
-source = "ADD R0, R1, R2"
-
-tokens = lexer.tokenize(source)
-
-print(tokens)
--- section_3/assembler/Pyarmasm/parser.py
+++ section_3/assembler/Pyarmasm/parser.py
@@ -1,4 +1,15 @@
+# from tokens -> AST 
+
 from lexer import Lexer
 
-lexer = Lexer()
+class Parser: 
+    def __init__(self, input) -> None:
+        self.lexer = Lexer()
+        self.tokens = self.lexer.tokenize(input)
 
+    def parse_instruction():
+
+    def parse_operand():
+
+
+    def parse(self):
